120bc52234cd6e4a56b56af67b63a1a0405c888c
secobb <design.serg@gmail.com>
2023-07-25 17:36:09 +0300

    Update class Soldier

--- src/Soldier.java
+++ src/Soldier.java
@@ -13,11 +13,23 @@ public class Soldier {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getRank() {
         return rank;
     }
 
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
     public String getGroup() {
         return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
     }
 }
